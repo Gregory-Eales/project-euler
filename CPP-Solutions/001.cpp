c8a8a5cf89a0e8ae1e066e97efc623287fcebe9c
Gregory Eales <gregoeales@aol.com>
2019-02-13 15:36:22 -0800

    added 001.cpp

--- CPP-Solutions/001.cpp
+++ CPP-Solutions/001.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
 #include <vector>
 
-
 using namespace std;
 
 
+// finds the sum of the multiples of 3, 5 up to a number
+int find_sum_multiples(int number){
 
-vector get_multiples(){
+	// initialize sum of multiples
+	int sum = 0;
+
+	// vectors for storing multiples
+	vector<int> multiples;
+
+	// loop through all numbers up to number
+	for(int i=1; i<number; i++){
+		if(i%3 == 0){
+			multiples.push_back(i);
+		}else if(i%5 == 0){
+			multiples.push_back(i);
+		}
+	}
+
+	// sum multiples
+	for(int j=0; j<=multiples.size(); j++){
+		sum = sum + multiples[j];
+	}
 	
+	return sum;
 }
 
 
-int main()
-{
-
-	
+int main(){
+	cout<<find_sum_multiples(1000)<<endl;
 	return 0;
 }
