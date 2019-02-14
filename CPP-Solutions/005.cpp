499ced32ccd2922f66d154eb3ad557df4b20ab4c
Gregory Eales <gregoeales@aol.com>
2019-02-14 10:51:00 -0800

    Create 005.cpp

---
+++ CPP-Solutions/005.cpp
@@ -0,0 +1,23 @@
+// what number is evenly divisable by
+// all numbers between 1 and 20
+
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+// find the first number evenly divisable by all
+// integers up to the value x
+bool evenly_divisable(int x){
+	cout<<"None"<<endl;
+	return 0;
+}
+
+
+
+
+
+int main(){
+	vector<int> x;
+
+}
