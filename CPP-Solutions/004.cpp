f2471a6a6fe1f99e8661659ab217594d4d3c5db6
Gregory Eales <gregoeales@aol.com>
2019-02-13 17:51:44 -0800

    finished 004.cpp

--- CPP-Solutions/004.cpp
+++ CPP-Solutions/004.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
-
-
-int largest_palindrome(){
-
-	for(int i=0; i<1000; i++){
-		for(int j=0;i<1000; j++){
-			break;
-		}
-	}
-	return 0;
-}
-
 
 bool is_palindrome(int number){
 	bool is_palindrome = 1;
@@ -31,20 +20,37 @@ bool is_palindrome(int number){
 
 	}else{
 		for(int i=0; i<= (num_length - 1)/2; i++){
-			if(num.at(i) != num.at(num_length - i)){
+			if(num.at(i) != num.at(num_length - 1 - i)){
 				is_palindrome = 0;
 				break;
 			}
+		}
 	}
 
 	return is_palindrome;
 }
 
+int largest_palindrome(){
+
+	for(int depth=1; depth<100;depth++){
+		cout<<depth<<endl;
+		for(int i=0; i<depth; i++){
+			int x = 1000 - i;
+			for(int j=0;j<30; j++){
+				int y = 1000 - j;
+				if(is_palindrome(x*y)){
+					return x*y;
+				}
+			}
+		}
+	}
+
+	
+	return 0;
+}
 
 
 int main(){
-	int x = 3331333;
-	string x_string = to_string(x);
-	cout<<x_string.at(1)<<endl;
+	cout<<largest_palindrome()<<endl;
 	return 0;
 }
