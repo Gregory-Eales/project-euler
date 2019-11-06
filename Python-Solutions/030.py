9a517d62b4dff409ac2e4fe87b8e1864e296fcd2
Gregory Eales <33496134+Gregory-Eales@users.noreply.github.com>
2019-11-06 03:26:59 -0800

    Create 030.py

---
+++ Python-Solutions/030.py
@@ -0,0 +1,19 @@
+from tqdm import tqdm
+import numpy as np
+
+def get_power_digits(n):
+    n = str(n)
+    n = [int(char)**5 for char in n]
+    n = np.sum(n)
+    return n
+
+
+def get_pow_nums(n):
+    nums = []
+    for i in tqdm(range(1, n)):
+        if i == get_power_digits(i):
+            nums.append(i)
+
+    return np.sum(nums)
+
+print(get_pow_nums(1000000))
