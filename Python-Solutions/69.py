ad374ef57de108be44edff49bf04c7f0c3e6cb56
Gregory Eales <gregoeales@aol.com>
2020-08-03 16:37:45 +0100

    Create 69.py

---
+++ Python-Solutions/69.py
@@ -0,0 +1,17 @@
+import math
+import numpy as np
+from tqdm import tqdm
+
+
+
+def get_primes(n):
+	pass
+
+def get_totient(n):
+	return n/get_relatives(n)
+
+def get_relatives(n):
+	return None
+
+def get_max_totient(n):
+	pass
